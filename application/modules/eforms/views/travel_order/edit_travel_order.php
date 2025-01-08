49fce2911d4780eb9db6acbd12cc784316e076d1
Harvey <juniordeveloper02@gccaggregates.com>
2025-01-08 08:39:44 +0800

    added identifier

--- application/modules/eforms/views/travel_order/edit_travel_order.php
+++ application/modules/eforms/views/travel_order/edit_travel_order.php
@@ -499,7 +499,7 @@
                 <div class="modal-body form">
                     <input type="hidden" value="<?php echo $_GET["id"] ? $_GET["id"]: 0; ?>" name="id"/>
                     <div class="form-group">
-                        <label class="control-label col-md-2">Remarks</label>
+                        <label class="control-label col-md-2 required">Remarks</label>
                         <div class="col-md-12">
                             <textarea name="approved_recommend_remarks"  class="form-control" rows="5" data-validation="required"></textarea> 
                         </div>
