c31738ce91fd37ba71fe4433e1e654ed18ee421b
momoypalaboy112 <jp04@gccaggregates.com>
2025-02-05 16:52:04 +0800

    change users lookup to emp_id

--- application/modules/login/controllers/Verifylogin.php
+++ application/modules/login/controllers/Verifylogin.php
@@ -40,7 +40,10 @@ class Verifylogin extends MY_Controller{
                 }
                 if (isset($query['auth']) && $query['auth'] == 1) {
                     $this->session->unset_userdata('logged_in');
-                    $userDetails = $this->db->select('u.email, u.telegram_chat_id, e.mobile_no')->from('gccmaster.tblusers u')->join('gccmaster.tblemployees e', 'u.emp_id = e.id', 'left')->where('u.id', $query['emp_id'])->get()->row_array();
+                    $userDetails = $this->db->select('u.email, u.telegram_chat_id, e.mobile_no')->
+                    from('gccmaster.tblusers u')->
+                    join('gccmaster.tblemployees e', 'u.emp_id = e.id', 'left')->
+                    where('u.emp_id', $query['emp_id'])->get()->row_array();
                     $userDetails = array_map(function($value) {
                         return is_null($value) ? '' : $value;
                     }, $userDetails);
