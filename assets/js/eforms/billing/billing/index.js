86a69767d1ad950631bf72cdd08bf03d25199cd5
Malvin Delacruz <jp05@gccph.com>
2025-03-14 10:55:30 +0800

    fixed export datatable

--- assets/js/eforms/billing/billing/index.js
+++ assets/js/eforms/billing/billing/index.js
@@ -6,168 +6,188 @@ let selectedReadingEndDate = null;
 let search_val = "";
 let query_builder = "";
 const tblBillings = $("#table-billing").DataTable({
-    dom: '<"toolbar">rtlip',
-    serverSide: true, 
-    processing: true,
-    aaSorting: [],
-    ajax: {
-        url: baseUrl("eforms/billing/get_billing_collection/"),
-        type: "post",
-        global: false,
-        dataType: "json",
-        data: function(d) {
-            d.csrf_token = _csrf_hash;
-            d.search['value'] = search_val;
-            d.query_builder = query_builder;
+  dom: '<"toolbar">rtlip',
+  serverSide: true, 
+  processing: true,
+  aaSorting: [],
+  ajax: {
+      url: baseUrl("eforms/billing/get_billing_collection/"),
+      type: "post",
+      global: false,
+      dataType: "json",
+      data: function(d) {
+          d.csrf_token = _csrf_hash;
+          d.search['value'] = search_val;
+          d.query_builder = query_builder;
 
-            if (selectedReadingStartDate && selectedReadingEndDate) {
-                d.startDate = moment(selectedReadingStartDate).format("YYYY-MM-DD");
-                d.endDate = moment(selectedReadingEndDate).format("YYYY-MM-DD");
-            } else {
-                d.startDate = '';
-                d.endDate = '';
-            }
+          if (selectedReadingStartDate && selectedReadingEndDate) {
+              d.startDate = moment(selectedReadingStartDate).format("YYYY-MM-DD");
+              d.endDate = moment(selectedReadingEndDate).format("YYYY-MM-DD");
+          } else {
+              d.startDate = '';
+              d.endDate = '';
+          }
+      }
+  },
+  searching: true,
+  columns: [
+      { data: "checkbox"},
+      { data: "ref_no", render: function (data) { return "<span class='m--font-boldest'>"+data+"</span>";} },
+      { data: "reading_ref_no" },
+      { data: "accountno"},
+      { data: "name", orderable: false},
+      { data: "meterno"},
+      { data: "billing_period", orderable: false, className: "text-center"},
+      { data: "due_date", className: "text-center"},
+      { 
+        data: "total_charges", className: "text-right", render: function (data) {
+            return "<span class='m--font-boldest'>"+numberWithCommas(data)+"</span>";
         }
-    },
-    searching: true,
-    columns: [
-        { data: "checkbox"},
-        { data: "ref_no", render: function (data) { return "<span class='m--font-boldest'>"+data+"</span>";} },
-        { data: "reading_ref_no" },
-        { data: "accountno"},
-        { data: "name", orderable: false},
-        { data: "meterno"},
-        { data: "billing_period", orderable: false, className: "text-center"},
-        { data: "due_date", className: "text-center"},
-        { 
-          data: "total_charges", className: "text-right", render: function (data) {
-              return "<span class='m--font-boldest'>"+numberWithCommas(data)+"</span>";
-          }
-        },
-        { data: "status", className: "text-center", render: function (data) {
-              return renderStatusDue(data);
-          }
-        },
-        { data: "print_count", width: "8%", className: "text-center", render: function (data) {
-              return renderStatusPrint(data);
-          }
-        },
-        { data: null, width: "5%", className: "text-center"},
-    ],
-    columnDefs: [
-        {
-            orderable: false,
-            className: 'select-checkbox',
-            targets: 0
-        },
-        {
+      },
+      { data: "status", className: "text-center", render: function (data) {
+            return renderStatusDue(data);
+        }
+      },
+      { data: "print_count", width: "8%", className: "text-center", render: function (data) {
+            return renderStatusPrint(data);
+        }
+      },
+      { data: null, width: "5%", className: "text-center"},
+  ],
+  columnDefs: [
+      {
           orderable: false,
-          targets: [8, 9]
-        },
-        {
-            data: null,
-            defaultContent: "",
-            targets: -1,
-            orderable: false,
+          className: 'select-checkbox',
+          targets: 0
+      },
+      {
+        orderable: false,
+        targets: [8, 9]
+      },
+      {
+          data: null,
+          defaultContent: "",
+          targets: -1,
+          orderable: false,
 
-            render: function ( data, type, row, meta ) { return itemDatatableActions(row); },
-        }, 
-        {
-            targets: "_all",
-            className: "v-middle",
-        }
-    ],
-    select: {
-        style:    'multi',
-        selector: 'td:first-child'
-    },
-    buttons: [
-        { 
-            extend: 'csv',
-            exportOptions: {
-                columns: "thead th:not(.notExport)"
-            },
-            // fieldBoundary: '',
-            customize: function (csv) {
-                let data = csv.split("\n"); // Split CSV into rows
-                
-                let targetUppercase = [1, 6]; // Columns to make uppercase
-                let targetTotalCharges = 5;
-                // Loop through each row
-                data = data.map((row, rowIndex) => {
-                    // Split row into columns, considering quoted fields
-                    let columns = row.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
-                
-                    columns = columns.map((col, columnIndex) => {
-                        col = col.trim(); // Remove extra spaces
-                
-                        if (rowIndex === 0) { 
-                            return col.replace(/\b\w/g, char => char.toUpperCase());
-                        }
-                
-                        if (targetUppercase.includes(columnIndex)) {
-                            col = col.toUpperCase(); // Convert to uppercase
-                        }
-                
-                        if (columnIndex === targetTotalCharges) {
-                            col = col.replace(/,/g, ''); // Remove commas
-                        }
-                
-                        return col;
-                    });
-                
-                    return columns.join(","); // Join modified columns
-                });
+          render: function ( data, type, row, meta ) { return itemDatatableActions(row); },
+      }, 
+      {
+          targets: "_all",
+          className: "v-middle",
+      }
+  ],
+  select: {
+      style:    'multi',
+      selector: 'td:first-child'
+  },
+  buttons: [
+      { 
+          extend: 'csv',
+          exportOptions: {
+              columns: "thead th:not(.notExport)"
+          },
+          // fieldBoundary: '',
+          customize: function (csv) {
+              let data = csv.split("\n"); // Split CSV into rows
+              
+              let targetUppercase = [1, 6]; // Columns to make uppercase
+              let targetTotalCharges = 5;
+              // Loop through each row
+              data = data.map((row, rowIndex) => {
+                  // Split row into columns, considering quoted fields
+                  let columns = row.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+              
+                  columns = columns.map((col, columnIndex) => {
+                      col = col.trim(); // Remove extra spaces
+              
+                      if (rowIndex === 0) { 
+                          return col.replace(/\b\w/g, char => char.toUpperCase());
+                      }
+              
+                      if (targetUppercase.includes(columnIndex)) {
+                          col = col.toUpperCase(); // Convert to uppercase
+                      }
+              
+                      if (columnIndex === targetTotalCharges) {
+                          col = col.replace(/,/g, ''); // Remove commas
+                      }
+              
+                      return col;
+                  });
+              
+                  return columns.join(","); // Join modified columns
+              });
 
-                return data.join("\n"); // Reassemble CSV
-            }
-        }, 
-        { 
-            extend: 'excel',
-            exportOptions: {
-                columns: "thead th:not(.notExport)"
-            }
-        }, 
-        { 
-            extend: 'pdf',
-            exportOptions: {
-                columns: "thead th:not(.notExport)"
-            }
-        },
-        {
-            text: 'Export Selected to Excel',
-            action: function ( e, dt, button, config ) {
-                let selectedData = dt.rows({ selected: true }).data().toArray();
-                let exportData = dt.buttons.exportData({
-                    columns: "thead th:not(.notExport)",
-                    modifier: {
-                        selected: true
+              return data.join("\n"); // Reassemble CSV
+          }
+      }, 
+      { 
+          extend: 'excel',
+          exportOptions: {
+              columns: "thead th:not(.notExport)"
+          },
+          customize: function (xlsx) {
+              let sheet = xlsx.xl.worksheets['sheet1.xml'];
+
+              // Convert Column B to Uppercase
+              $('row:not(:nth-child(2)) c[r^="B"]', sheet).each(function () {
+                  let cell = $(this).find('is t, v'); // Find the text inside
+                  let text = cell.text().trim(); // Get the existing text
+
+                  if (text) {
+                      cell.text(text.toUpperCase()); // Convert to uppercase
+                  }
+              });
+          }
+      }, 
+      {
+          extend: 'pdf',
+          exportOptions: {
+              columns: "thead th:not(.notExport)"
+          },
+          orientation: 'landscape',
+          pageSize: 'LEGAL',
+          customize: function (doc) {
+              // Set dynamic widths for all columns
+              let columnWidths = new Array(doc.content[1].table.body[0].length).fill('*');
+
+              // Define custom widths for specific columns (adjust index as needed)
+              columnWidths[1] = '20%';
+
+              // Apply column widths
+              doc.content[1].table.widths = columnWidths;
+              
+              // Loop through table body and target specific column
+              doc.content[1].table.body.forEach(function (row, rowIndex) {
+                  if (rowIndex === 0) { return; } // Skip the header row
+
+                  let targetUppercase = [1, 6]; // Columns to make uppercase
+                  let targetCenter = [0, 2, 3, 4, 6]; // Columns to center align
+                  let targetRight = 5; // Column to right align
+
+                  row.forEach((cell, columnIndex) => {
+                    if (!cell.text) { return; }
+
+                    if (targetUppercase.includes(columnIndex)) {
+                        cell.text = cell.text.toUpperCase();
                     }
-                });
 
-                // Create a new DataTable instance for exporting
-                let exportTable = $('<table>').DataTable({
-                    data: selectedData,
-                    columns: dt.settings().init().columns,
-                    dom: 'Bfrtip',
-                    buttons: [
-                        {
-                          extend: 'excel',
-                          exportOptions: {
-                              columns: "thead th:not(.notExport)"
-                          }
-                        }
-                    ] 
-                });
+                    if (targetCenter.includes(columnIndex)) {
+                        cell.alignment = 'center';
+                    }
 
-                // Trigger the export
-                exportTable.button('.buttons-excel').trigger();
-            }
-        }
-    ],
-    createdRow: function(row, data, dataIndex) {
-        $(row).find('td').addClass('v-middle');
-    }
+                    if (columnIndex === targetRight) {
+                        cell.alignment = 'right';
+                    }
+                  });
+              });
+          }
+      },
+  ],
+  createdRow: function(row, data, dataIndex) {
+      $(row).find('td').addClass('v-middle');
+  }
 });
 
 $('#select_all_bills').on('change', function() {
