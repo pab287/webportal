8bd1de3c469936d9232889d6eae8a468652cedca
momoypalaboy112 <jp04@gccaggregates.com>
2025-03-18 16:29:32 +0800

    fix scroll

--- application/modules/hris/views/index.php
+++ application/modules/hris/views/index.php
@@ -514,7 +514,7 @@
         </div>
         <div class="m-portlet__body">
             <div class="tab-content" style="height: 600px; overflow-y: auto;">
-                <div class="tab-pane active overflow-auto" id="m_portlet_tab_1_1" style="max-height: 400px;">
+                <div class="tab-pane active overflow-auto" id="m_portlet_tab_1_1">
                     <div class="demographics-avatar-container">
                     <?php foreach ($newly_hired as $row) {
                         $image_path = "uploads/files/images/employee_files/empcode_" . $row->id . "/" . $row->pic_filename;
