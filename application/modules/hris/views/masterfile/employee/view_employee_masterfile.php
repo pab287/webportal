c18dcd2b0fcc6e0c347ec5b8f766f4e8ccfed6d9
Harvey <juniordeveloper02@gccaggregates.com>
2025-03-25 17:10:05 +0800

    fixed pagination and search in payroll data sheet in profile

--- application/modules/hris/views/masterfile/employee/view_employee_masterfile.php
+++ application/modules/hris/views/masterfile/employee/view_employee_masterfile.php
@@ -409,133 +409,136 @@
 </div>
 
 <?php if(isset($profile_payroll_sheet, $show_payroll_payslip) && $show_payroll_payslip && $profile_payroll_sheet && (isset($payroll_sheet_data) && is_array($payroll_sheet_data) && count($payroll_sheet_data) > 0)): ?>
-<div id="temp-payslip_content--container">
-<?php $this->load->view("core/profile/modals/payroll_payslip"); ?>
-<script>
-    const viewPayrollPayslipModal = $("#view-payroll-payslip-modal");
-    var vmPayslipContent = new Vue({
-        el: "#temp-payslip_content",
-        data: { row: {} },
-    });
+    <div id="temp-payslip_content--container">
+        <?php $this->load->view("core/profile/modals/payroll_payslip"); ?>
+    </div>
+    <script>
+        const viewPayrollPayslipModal = $("#view-payroll-payslip-modal");
+        var vmPayslipContent = new Vue({
+            el: "#temp-payslip_content",
+            data: { row: {} },
+        });
 
-    $("#table-payroll_sheet-payslip").DataTable({ ordering: false });
-    $("#table-payroll_sheet-payslip_filter input[type='search']").removeClass("form-control-sm");
-    const getPayrollSheetData = function(id){
-        if(id){
-            $.get(siteUrl('core/profile/get_payroll_sheet_data'), { id: id }, "json")
-            .done(function(data){
-                const json = JSON.parse(data);
-                let tempRow = json.response ? Object.assign({}, json.data) : {};
-                vmPayslipContent.row = Object.assign({}, tempRow);
+        $(document).ready( function () {
+            $("#table-payroll_sheet-payslip").DataTable({ ordering: false });
+            $("#table-payroll_sheet-payslip_filter input[type='search']").removeClass("form-control-sm");
+        });
+        
+        const getPayrollSheetData = function(id){
+            if(id){
+                $.get(siteUrl('core/profile/get_payroll_sheet_data'), { id: id }, "json")
+                .done(function(data){
+                    const json = JSON.parse(data);
+                    let tempRow = json.response ? Object.assign({}, json.data) : {};
+                    vmPayslipContent.row = Object.assign({}, tempRow);
 
-                var data = json.data;
-                var totalOT = parseFloat(vmPayslipContent.row.ot_amount) + parseFloat(vmPayslipContent.row.ot_ndiff_amount);
-                var totalOTHrs = (parseFloat(vmPayslipContent.row.ot_minutes) + parseFloat(vmPayslipContent.row.ot_ndiff_minutes)) / 60;
-                vmPayslipContent.total_ot_hrs = numberFormat(totalOTHrs);
-                vmPayslipContent.ot_hrs = numberFormat(parseFloat(vmPayslipContent.row.ot_minutes)/60);
-                vmPayslipContent.ot_computation = numberFormat(totalOT);
-                vmPayslipContent.ot_ndiff_hrs = numberFormat(parseFloat(vmPayslipContent.row.ot_ndiff_minutes) / 60);
-                vmPayslipContent.ot_ndiff_computation = numberFormat(parseFloat(vmPayslipContent.row.ot_ndiff_amount));
+                    var data = json.data;
+                    var totalOT = parseFloat(vmPayslipContent.row.ot_amount) + parseFloat(vmPayslipContent.row.ot_ndiff_amount);
+                    var totalOTHrs = (parseFloat(vmPayslipContent.row.ot_minutes) + parseFloat(vmPayslipContent.row.ot_ndiff_minutes)) / 60;
+                    vmPayslipContent.total_ot_hrs = numberFormat(totalOTHrs);
+                    vmPayslipContent.ot_hrs = numberFormat(parseFloat(vmPayslipContent.row.ot_minutes)/60);
+                    vmPayslipContent.ot_computation = numberFormat(totalOT);
+                    vmPayslipContent.ot_ndiff_hrs = numberFormat(parseFloat(vmPayslipContent.row.ot_ndiff_minutes) / 60);
+                    vmPayslipContent.ot_ndiff_computation = numberFormat(parseFloat(vmPayslipContent.row.ot_ndiff_amount));
 
-                let tempLoan = [];
-                let tempOthers = [];
-                let totalLoan = parseFloat(vmPayslipContent.row.totalLoan.replace(/,/g, ''));
-                let totalDeduction = 0;
-                let totalOthersDeductions = 0;
-                let overAllTotal = 0;
-                const tempCreatedAdjustments = data.created_adjustments;
+                    let tempLoan = [];
+                    let tempOthers = [];
+                    let totalLoan = parseFloat(vmPayslipContent.row.totalLoan.replace(/,/g, ''));
+                    let totalDeduction = 0;
+                    let totalOthersDeductions = 0;
+                    let overAllTotal = 0;
+                    const tempCreatedAdjustments = data.created_adjustments;
 
-                if (data.sss && parseFloat(data.sss) > 0) {
-                    totalDeduction = totalDeduction + parseFloat(data.sss);
-                }
+                    if (data.sss && parseFloat(data.sss) > 0) {
+                        totalDeduction = totalDeduction + parseFloat(data.sss);
+                    }
 
-                if (data.sss_prov && parseFloat(data.sss_prov) > 0) {
-                    totalDeduction = totalDeduction + parseFloat(data.sss_prov);
-                }
-                
-                if (data.ph && parseFloat(data.ph) > 0) {
-                    totalDeduction = totalDeduction + parseFloat(data.ph);
-                }
-                
-                if (data.hdmf && parseFloat(data.hdmf) > 0) {
-                    totalDeduction = totalDeduction + parseFloat(data.hdmf);
-                }
-                
-                if (data.tax && parseFloat(data.tax) > 0) {
-                    totalDeduction = totalDeduction + parseFloat(data.tax);
-                }
+                    if (data.sss_prov && parseFloat(data.sss_prov) > 0) {
+                        totalDeduction = totalDeduction + parseFloat(data.sss_prov);
+                    }
+                    
+                    if (data.ph && parseFloat(data.ph) > 0) {
+                        totalDeduction = totalDeduction + parseFloat(data.ph);
+                    }
+                    
+                    if (data.hdmf && parseFloat(data.hdmf) > 0) {
+                        totalDeduction = totalDeduction + parseFloat(data.hdmf);
+                    }
+                    
+                    if (data.tax && parseFloat(data.tax) > 0) {
+                        totalDeduction = totalDeduction + parseFloat(data.tax);
+                    }
 
-                $.each(json.data.loans, function (index, item) {
-                    if (item.loan_name.toLowerCase() != 'charges') {
-                        var temp_amount = parseFloat(item.amount_due);
+                    $.each(json.data.loans, function (index, item) {
+                        if (item.loan_name.toLowerCase() != 'charges') {
+                            var temp_amount = parseFloat(item.amount_due);
 
-                        // for adding cash advance with loan adjustments
-                        if (typeof item.loan_name !== "undefined" && item.loan_name.toLowerCase() == 'cash advance') {
-                            temp_amount = parseFloat(item.amount_due.replace(/,/g, ''));
+                            // for adding cash advance with loan adjustments
+                            if (typeof item.loan_name !== "undefined" && item.loan_name.toLowerCase() == 'cash advance') {
+                                temp_amount = parseFloat(item.amount_due.replace(/,/g, ''));
 
-                            if (typeof tempCreatedAdjustments !== "undefined" && tempCreatedAdjustments) {
-                                const created_adjustments = tempCreatedAdjustments.split(",");
-                                var tempAdj = 0;
-                                created_adjustments.forEach((row, i) => {
-                                    const temp_adjustment = row.split("||");
-                                    const adj_type = parseInt(temp_adjustment[2]);
-                                    const temp_status = parseInt(temp_adjustment[3]);
-                                    let _temp = parseFloat(item.amount_due);
-                                    if (adj_type == 1) {
-                                        _temp = parseFloat(temp_amount) + parseFloat(temp_adjustment[1]);
-                                    } else {
-                                        _temp = parseFloat(temp_amount) - parseFloat(temp_adjustment[1]);
-                                    }
+                                if (typeof tempCreatedAdjustments !== "undefined" && tempCreatedAdjustments) {
+                                    const created_adjustments = tempCreatedAdjustments.split(",");
+                                    var tempAdj = 0;
+                                    created_adjustments.forEach((row, i) => {
+                                        const temp_adjustment = row.split("||");
+                                        const adj_type = parseInt(temp_adjustment[2]);
+                                        const temp_status = parseInt(temp_adjustment[3]);
+                                        let _temp = parseFloat(item.amount_due);
+                                        if (adj_type == 1) {
+                                            _temp = parseFloat(temp_amount) + parseFloat(temp_adjustment[1]);
+                                        } else {
+                                            _temp = parseFloat(temp_amount) - parseFloat(temp_adjustment[1]);
+                                        }
 
-                                    tempAdj = _temp;
-                                    _temp = formatNumber(_temp);
+                                        tempAdj = _temp;
+                                        _temp = formatNumber(_temp);
 
-                                    if (temp_adjustment[0] == "LOAN" && temp_status === 1) {
-                                        temp_amount = _temp;
-                                    }
-                                });
+                                        if (temp_adjustment[0] == "LOAN" && temp_status === 1) {
+                                            temp_amount = _temp;
+                                        }
+                                    });
+                                }
                             }
+
+                            tempLoan.push({
+                                'loan_name' : item.loan_name,
+                                'amount_due' : temp_amount,
+                                'loan_type' : item.loan_type
+                            });
                         }
 
-                        tempLoan.push({
-                            'loan_name' : item.loan_name,
-                            'amount_due' : temp_amount,
-                            'loan_type' : item.loan_type
-                        });
-                    }
+                        // for adding the charges to Other Deductions
+                        if (item.loan_name.toLowerCase() == 'charges') {
+                            vmPayslipContent.row.adjustment_deductions.push({
+                                'label' : item.loan_name,
+                                'display_value' : item.amount_due,
+                                'value' : item.amount_due,
+                                'adj_type' : 0
+                            });
 
-                    // for adding the charges to Other Deductions
-                    if (item.loan_name.toLowerCase() == 'charges') {
-                        vmPayslipContent.row.adjustment_deductions.push({
-                            'label' : item.loan_name,
-                            'display_value' : item.amount_due,
-                            'value' : item.amount_due,
-                            'adj_type' : 0
-                        });
+                            totalLoan = totalLoan - parseFloat(item.amount_due.replace(/,/g, ''));
+                        }
+                    });
 
-                        totalLoan = totalLoan - parseFloat(item.amount_due.replace(/,/g, ''));
-                    }
+                    $.each(vmPayslipContent.row.adjustment_deductions, function (index, item) {
+                        totalOthersDeductions = totalOthersDeductions + parseFloat(item.display_value.replace(/,/g, ''));
+                    });
+
+                    console.log(totalDeduction, totalLoan, totalOthersDeductions);
+                    overAllTotal = parseFloat(totalDeduction) + parseFloat(totalLoan) + parseFloat(totalOthersDeductions) + parseFloat(vmPayslipContent.row.total_loans_interest);
+
+                    vmPayslipContent.row.loans = tempLoan;
+                    vmPayslipContent.row.totalLoan = numberFormat(totalLoan);
+                    vmPayslipContent.row.total_allowances = numberFormat(vmPayslipContent.row.total_allowances);
+                    vmPayslipContent.row.deductions = numberFormat(totalDeduction);
+                    vmPayslipContent.row.total_others_deductions = numberFormat(totalOthersDeductions);
+                    vmPayslipContent.row.overall_total_deductions = numberFormat(overAllTotal);
+                    vmPayslipContent.row.adjustment_d_count = vmPayslipContent.row.adjustment_deductions.length;
+
+                    if(json.response){ viewPayrollPayslipModal.modal("show"); }
                 });
-
-                $.each(vmPayslipContent.row.adjustment_deductions, function (index, item) {
-                    totalOthersDeductions = totalOthersDeductions + parseFloat(item.display_value.replace(/,/g, ''));
-                });
-
-                console.log(totalDeduction, totalLoan, totalOthersDeductions);
-                overAllTotal = parseFloat(totalDeduction) + parseFloat(totalLoan) + parseFloat(totalOthersDeductions) + parseFloat(vmPayslipContent.row.total_loans_interest);
-
-                vmPayslipContent.row.loans = tempLoan;
-                vmPayslipContent.row.totalLoan = numberFormat(totalLoan);
-                vmPayslipContent.row.total_allowances = numberFormat(vmPayslipContent.row.total_allowances);
-                vmPayslipContent.row.deductions = numberFormat(totalDeduction);
-                vmPayslipContent.row.total_others_deductions = numberFormat(totalOthersDeductions);
-                vmPayslipContent.row.overall_total_deductions = numberFormat(overAllTotal);
-                vmPayslipContent.row.adjustment_d_count = vmPayslipContent.row.adjustment_deductions.length;
-
-                if(json.response){ viewPayrollPayslipModal.modal("show"); }
-            });
+            }
         }
-    }
-</script>
-</div>
+    </script>
 <?php endif; ?>
