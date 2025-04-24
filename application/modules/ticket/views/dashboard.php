cf85be40bb105aa8a42f880efa67c803f183dba0
momoypalaboy112 <jp04@gccaggregates.com>
2025-04-24 09:52:35 +0800

    ave time completed

--- application/modules/ticket/views/dashboard.php
+++ application/modules/ticket/views/dashboard.php
@@ -94,7 +94,7 @@
 						<div class="m-portlet__head-caption">
 							<div class="m-portlet__head-title">
 								<h3 class="m-portlet__head-text">
-									Average Time to Resolve
+									Average Time Completed
 								</h3>
 							</div>
 						</div>
