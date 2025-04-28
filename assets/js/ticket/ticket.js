9706afa27cc5d61d2d6f261969c058af87d88f05
momoypalaboy112 <jp04@gccaggregates.com>
2025-04-28 17:02:24 +0800

    updated completed ticket

--- assets/js/ticket/ticket.js
+++ assets/js/ticket/ticket.js
@@ -95,7 +95,7 @@ let tbl = $("#table-tickets").DataTable({
         {
             data: null,
             render: function (data, type, row) {
-                if(row.status == 'Completed' || row.status == 'RESOLVED') {
+                if(row.status == 'completed' || row.status == 'RESOLVED') {
                     return 'Ticket Completed';
                 }
                 if (!row.requested_date) return '---';
