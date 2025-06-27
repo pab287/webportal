76db79dc2fad255b4b54f7eec13059a09eb46027
momoypalaboy112 <jp04@gccaggregates.com>
2025-06-27 15:20:59 +0800

    fix upper

--- application/modules/hris/models/Employee_model.php
+++ application/modules/hris/models/Employee_model.php
@@ -3723,7 +3723,8 @@
                         }
                         // END
         
-                        // $post = array_map('strtoupper', $post);
+                        $post = array_map('strtoupper', $post);
+                        $post['liscert_attachment'] = strtolower($post['liscert_attachment']);
                         $saved = $this->db->insert($this->employeeLicensureTable, $post);
                         $fullname = $this->getEmployeeName($post['emp_id']);
                         if ($saved) {
