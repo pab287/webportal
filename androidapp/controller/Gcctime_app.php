9d325f0a08f6fc3bf5d614e21c79f2812d405fea
Rockefeller Ayuco <juniordeveloper06@gccaggregates.com>
2025-08-07 17:48:44 +0800

    gcctime311 version

---
+++ androidapp/controller/Gcctime_app.php
@@ -0,0 +1,86 @@
+<?php
+// defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Gcctime_app extends Dbase{
+        private $GCCTIME_EFORM;
+
+        public function __construct(){
+            $this->GCCTIME_EFORM = new Gcctime_app_m();
+        }
+            
+        public function gcctime_eform_login(){
+            $data = $this->GCCTIME_EFORM->gcctimeEformLogin();
+            echo $data;
+        }
+
+        public function is_signout(){
+            $data = $this->GCCTIME_EFORM->isSignout();
+            echo $data;
+        }
+        
+        public function portal_modules(){
+            $data = $this->GCCTIME_EFORM->portalModules();
+            echo $data;
+        }
+
+        public function user_privilege(){
+            $data = $this->GCCTIME_EFORM->userPrivilege();
+            echo $data;
+        }
+
+        public function get_geofence(){
+            $data = $this->GCCTIME_EFORM->getGeofence();
+            echo $data;
+        }
+        public function get_geofencev311(){
+            $data = $this->GCCTIME_EFORM->getGeofencev311();
+            echo $data;
+        }
+
+        public function fetch_attendance(){
+            $data = $this->GCCTIME_EFORM->fetchAttendance();
+            echo $data;
+        }
+        public function fetch_attendancev311(){
+            $data = $this->GCCTIME_EFORM->fetchAttendancev311();
+            echo $data;
+
+        }
+        public function time_log_offline(){
+            $data = $this->GCCTIME_EFORM->timeLogOffline();
+            echo $data;
+        }
+        public function time_log_offlinev311(){
+            $data = $this->GCCTIME_EFORM->timeLogOfflinev311();
+            echo $data;
+        }
+
+        public function time_log(){
+            $data = $this->GCCTIME_EFORM->timeLog();
+            echo $data;
+        }
+        public function time_logv311(){
+            $data = $this->GCCTIME_EFORM->timeLogv311();
+            echo $data;
+        }
+
+        public function invalid_action_logv311(){
+            $data = $this->GCCTIME_EFORM->invalidActLogv311();
+            echo $data;
+        }
+        public function offline_invalid_action_logv311(){
+            $data = $this->GCCTIME_EFORM->offlineInvalidActLogv311();
+            echo $data;
+        }
+
+        public function gcctime_eform_loginv311(){
+            $data = $this->GCCTIME_EFORM->gcctimeEformLoginv311();
+            echo $data;
+        }
+        public function app_versionv311(){
+            $data = $this->GCCTIME_EFORM->appVersionv311();
+            echo $data;
+        }
+
+    }
+?>
