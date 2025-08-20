8a85b76e439126dcc3339c7ac3d2372efeb489aa
momoypalaboy112 <jp04@gccaggregates.com>
2025-08-20 09:17:05 +0800

    changes

--- application/modules/users/models/User_model.php
+++ application/modules/users/models/User_model.php
@@ -560,6 +560,7 @@ class User_model extends CI_Model{
         $id = $post['id'];
         $this->db->trans_start();
         $sendOtp = $this->sendOTP($id);
+        var_dump($sendOtp);
         if (!$sendOtp['sent_sms'] && !$sendOtp['sent_email']) {
             $resultset['message'] = $sendOtp['message'];
             $this->db->trans_rollback();
@@ -630,10 +631,11 @@ class User_model extends CI_Model{
         if ($result->mobile_no) {
             $message = "[GC&C] Your Conyxph account recovery code is: $OTP. For security reasons, do not share this code with anyone. " .
                        "If you did not request this, please ignore this message.";
-            $response['sent_sms'] = $this->gateway->sendPlaySMS($result->mobile_no, $message);
+            $response['sent_sms'] = $this->gateway->sendPlaySMS($result->mobile_no, $message)->status;
+            $response['mobile_no'] = $result->mobile_no;
         }
         
-        $response['sent_email'] = $this->core_layout->send_email('core','GC & C Conyx PH','Account Recovery',$email_content,$mailer);
+        // $response['sent_email'] = @$this->core_layout->send_email('core','GC & C Conyx PH','Account Recovery',$email_content,$mailer);
         return $response;
     }
 
@@ -645,7 +647,6 @@ class User_model extends CI_Model{
             ->where('id', $id)
             ->from('gccmaster.tblusers')
             ->get()->row();
-            // var_dump($user->waive_password_update);
             if($user->is_important == 1){
                 $new_last_update = ($user->waive_password_update == 0) ? date('Y-m-d H:i:s', strtotime('+60 days')) : date('Y-m-d H:i:s', strtotime('+5 days'));
             }
