36f06c0db85b37cd68539a555245f5525c016825
momoypalaboy112 <jp04@gccaggregates.com>
2025-08-29 17:54:29 +0800

    added participang page

--- application/modules/hris/views/masterfile/calendar/calendar_of_events/participants_page.php
+++ application/modules/hris/views/masterfile/calendar/calendar_of_events/participants_page.php
@@ -1,17 +1,47 @@
+<div class="row ">
+    <div class="col-12 ">
+        <div class="m-content">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="m-portlet" id="m_portlet">
+                        <div class="m-portlet__head">
+                            <div class="m-portlet__head-caption">
+                                <div class="m-portlet__head-title">
+                                    <span class="m-portlet__head-icon">
+                                        <i class="la la-calendar"></i>
+                                    </span>
+                                    <h3 class="m-portlet__head-text">
+                                        Event Details
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-portlet__body">
 
-<div class="m-content">
-    <div class="m-portlet" id="m_portlet">
-        <div class="m-portlet__head">
-            <div class="m-portlet__head-caption">
-                <div class="m-portlet__head-title">
-                    <span class="m-portlet__head-icon">
-                        <i class="flaticon-calendar-2"></i>
-                    </span>
-                    <h3 class="m-portlet__head-text">
-                        Company Events
-                    </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8 col-sm-12">
+                    <div class="m-portlet" id="m_portlet">
+                        <div class="m-portlet__head">
+                            <div class="m-portlet__head-caption">
+                                <div class="m-portlet__head-title">
+                                    <span class="m-portlet__head-icon">
+                                        <i class="la la-user"></i>
+                                    </span>
+                                    <h3 class="m-portlet__head-text">
+                                        Event Participants
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-portlet__body">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
