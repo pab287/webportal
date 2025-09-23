65016e02b4a9b2b206fc642b1aa5ac3553a3dbb6
momoypalaboy112 <jp04@gccaggregates.com>
2025-09-23 13:28:49 +0800

    added attachment viewing

--- assets/js/hris/profile_view_script.js
+++ assets/js/hris/profile_view_script.js
@@ -244,6 +244,23 @@ let employeeDataSheet = new Vue({
                 this.main = { ...this.$data.main, ..._tempContentData.data.main };
             }
         },
+        formatDateRange(from, to) {
+            if (!from || !to) return "";
+            const start = moment(from);
+            const end = moment(to);
+            if (start.isSame(end, "day")) {
+              return start.format("MMM DD, YYYY");
+            } 
+            else if (start.isSame(end, "month")) {
+              return `${start.format("MMM DD")}–${end.format("DD, YYYY")}`;
+            } 
+            else if (start.isSame(end, "year")) {
+              return `${start.format("MMM DD")} – ${end.format("MMM DD, YYYY")}`;
+            } 
+            else {
+              return `${start.format("MMM DD, YYYY")} – ${end.format("MMM DD, YYYY")}`;
+            }
+          },
         calculateAge(birthdate) {
             if (!birthdate || birthdate == '0000-00-00') return '---';
             const today = new Date();
@@ -385,12 +402,15 @@ let employeeDataSheet = new Vue({
                 }
             });
         },
-        openFile(name,offense=true) {
+        openFile(name,offense=true,seminars=false) {
             let tab;
             if (offense){
                 tab = "offenses_commendation";
             }else{
                 tab = "licenses_certificates";
+            }
+            if(seminars){
+                tab = "trainings";
             }
             let fileUrl = baseUrl("uploads/files/documents/employee_files/empcode_" + id + "/"+tab+"/" + encodeURIComponent(name));
             console.log(fileUrl);
