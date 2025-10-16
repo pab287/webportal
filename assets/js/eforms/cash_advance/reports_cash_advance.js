051b7519912857c76375493731f2d6fd87c47d2e
juniordeveloper02 <juniordeveloper02@gccaggregates.com>
2025-10-16 09:22:37 +0800

    revert med loan

--- assets/js/eforms/cash_advance/reports_cash_advance.js
+++ assets/js/eforms/cash_advance/reports_cash_advance.js
@@ -12,7 +12,7 @@ let = tblCashAdvanceReport = $('#cash_advance_reports')
                     extend: 'excelHtml5',
                     title: 'CASH ADVANCE REPORTS',
                     exportOptions: {
-                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16],
+                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16],
                         format: {
                             body: function(data, row, column, node) {
                                 return data.toString().replace(/<[^>]*>/g, '').toUpperCase();
@@ -31,7 +31,7 @@ let = tblCashAdvanceReport = $('#cash_advance_reports')
                     extend: 'pdfHtml5',
                     title: 'CASH ADVANCE REPORTS',
                     exportOptions: {
-                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16],
+                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16],
                         format: {
                             body: function(data, row, column, node) {
                                 return data.toString().replace(/<[^>]*>/g, '').toUpperCase();
@@ -87,7 +87,7 @@ let = tblCashAdvanceReport = $('#cash_advance_reports')
                     extend: 'print',
                     title: 'CASH ADVANCE REPORTS',
                     exportOptions: {
-                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16],
+                        columns: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16],
                         format: {
                             body: function(data, row, column, node) {
                                 return data.toString().replace(/<[^>]*>/g, '').toUpperCase();
@@ -160,6 +160,11 @@ let = tblCashAdvanceReport = $('#cash_advance_reports')
             { data: 'company', visible: false, title: 'COMPANY', },
             { data: 'reference_no', visible: false, title: 'REF NO' },
             { data: 'amt_approved', orderable: false, title: 'AMOUNT APPROVED' ,
+                render: function(data) {
+                    return Number(data).toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
+                }
+            },
+            { data: "med_loan", orderable: false,  title: 'MEDICAL LOAN',
                 render: function(data) {
                     return Number(data).toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
                 }
