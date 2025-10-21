ab993a5b5a6ae2c55a165bb78b99601532e112ad
momoypalaboy112 <jp04@gccaggregates.com>
2025-10-21 17:38:00 +0800

    fix awarding

--- application/modules/events/models/Events_model.php
+++ application/modules/events/models/Events_model.php
@@ -668,6 +668,7 @@ class Events_model extends MX_Controller {
                 $resultset["response"] = true;
                 $insertId = $this->db->insert_id();
                 $this->db->where('emp_id', $post['emp_id']);
+                $this->db->where('event_id', $eventId);
                 $this->db->update('gcchris.events_participants', [
                     'cert_awarded' => $insertId
                 ]);
