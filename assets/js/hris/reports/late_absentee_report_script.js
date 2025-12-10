d5936f79f710a1137683012bd6b07dbb5b5931c7
Paul Andre Balayo <jp01@gccph.com>
2025-12-10 16:12:36 +0800

    updates on abasentee report

--- assets/js/hris/reports/late_absentee_report_script.js
+++ assets/js/hris/reports/late_absentee_report_script.js
@@ -343,6 +343,7 @@ $.validate({
             success: function(json){
                 if(json.response){
                     typeReport =  document.querySelector('input[name="report_type"]:checked').value;
+                    console.log(json.data);
                     dtTableLateAbsenteeReport.clear();
                     dtTableLateAbsenteeReport.rows.add(json.data);
                     dtTableLateAbsenteeReport.draw(false);
@@ -446,7 +447,7 @@ if(typeof dtTableLateAbsentee !== "undefined" && dtTableLateAbsentee.length > 0)
             { title: "Total", data: "reports_total", width: "8%", className: "text-right" },
             { title: "", width: "6%", className: "text-center", render: function(_data, _type, row){
                 let cleanedRow = {};
-                for (let key in row) { cleanedRow[key] = String(row[key]).replace(/[^a-zA-Z0-9 \.,\-\_:]/g, ''); }
+                for (let key in row) { cleanedRow[key] = String(row[key]).replace(/[^a-zA-Z0-9 .,~\-_:\/]/g, ''); }
                 const objResponse = JSON.stringify(cleanedRow);
                 return `<button class='btn btn-secondary m-btn m-btn--icon btn-sm m-btn--icon-only m-btn--pill btnView btnLateAbsenteePreview' data-raw='${objResponse}'>
                     <i class='fa fa-hourglass-half'></i>
