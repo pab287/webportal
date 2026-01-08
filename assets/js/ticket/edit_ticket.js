ee1ab746ad7a9284916e312af2e807f4c54fb7dc
momoypalaboy112 <jp04@gccaggregates.com>
2026-01-08 16:24:56 +0800

    removed console

--- assets/js/ticket/edit_ticket.js
+++ assets/js/ticket/edit_ticket.js
@@ -144,7 +144,7 @@ $.ajax({
         vmTab1.vm_tab1 = Object.assign({}, data);
 
         const categories = (vmTab1.vm_tab1.category || '').toLowerCase();
-        console.log(categories);
+
         if (categories === "payroll") {
             performed_by = _tempContentData.performed_by_payroll;
         }
@@ -154,7 +154,7 @@ $.ajax({
         else {
             performed_by = _tempContentData.performed_by;
         }
-        console.log(performed_by);
+
         $("#performed_by").select2({
             width: "100%",
             placeholder: "Select an option",
@@ -236,8 +236,6 @@ $.ajax({
         if(data.status == 'open'){
             _tempContentData.status = [{'text': 'open', 'id': 'open'},{'text': 'In Progress', 'id': 'in progress'}];
         }
-
-        console.log(data);
 
         $("#status").select2({
             width: "100%",
@@ -588,7 +586,6 @@ function removeDocument(el, filename) {
                         images = images.filter((n) => {return n != _name});
                         $(parent).remove();
                         $("#pic").val(images);
-                        console.log(images)
                         $("#remove-file-confirmation-modal").modal("hide");
                         toastr.success(_name,"Removed File", 5000);
                     } else {
