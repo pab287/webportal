c7f2e185fb146227b127a6c0cb11a7e1b3efc3b9
juniordeveloper02 <juniordeveloper02@gccaggregates.com>
2026-02-03 12:47:04 +0800

    added deductions in profile

--- application/modules/core/controllers/Profile.php
+++ application/modules/core/controllers/Profile.php
@@ -32,9 +32,6 @@ class Profile extends MY_Controller {
 		$this->core_layout->addJs("plugins/fileupload/js/jquery.iframe-transport.js");
 		$this->core_layout->addJs("plugins/fileupload/js/jquery.fileupload.js");
 
-		$this->core_layout->addCss('global/plugins/swal/sweetalert2.min.css', true);
-        $this->core_layout->addJs('global/plugins/swal/sweetalert2.all.min.js', true);
-
 		$this->core_layout->addCss("css/hris/view_employee_masterfile.css", true);
 		$this->core_layout->addCss("plugins/star-rating/css/star-rating-svg.css", true);
 		$this->core_layout->addCss('css/hris/index.css', true);
@@ -45,10 +42,12 @@ class Profile extends MY_Controller {
 		$showPayrollPayslip = is_array($currentActions) && count($currentActions) > 0 && in_array("view_own_request", $currentActions);
 
 		$data = $this->utilities->parseFormDataToObject(array("data" => $this->employee_model->getEmployeeDataDetails($employee_id),
-			"profile_payroll_sheet"=>true,
-			"payroll_sheet_data"=>$this->get_employee_payroll_data($employee_id),
-			"payroll_sheet_max_id"=>$this->get_max_employee_payroll_data($employee_id),
-			"show_payroll_payslip"=>$showPayrollPayslip));
+		"profile_payroll_sheet"=>true,
+		"payroll_sheet_data"=>$this->get_employee_payroll_data($employee_id),
+		"payroll_sheet_max_id"=>$this->get_max_employee_payroll_data($employee_id),
+		"show_payroll_payslip"=>$showPayrollPayslip,
+		"deductions" => $this->profile->get_employee_deductions($employee_id)));
+
 		$data->tab ='personalInfo';
 		$data->page = 'profile'; //added to display the sms notification to profile only because the 201 and profile shares the same view file
 		$data->questions_list = $this->employee_model->getQuestionsList();
