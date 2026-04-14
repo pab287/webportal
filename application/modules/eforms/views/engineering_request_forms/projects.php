7308c4da3aaa502355e5eeeaf250c64068caf0ed
momoypalaboy112 <jp04@gccaggregates.com>
2026-04-14 14:49:23 +0800

    back to masterfile button

--- application/modules/eforms/views/engineering_request_forms/projects.php
+++ application/modules/eforms/views/engineering_request_forms/projects.php
@@ -10,7 +10,7 @@
             </div>
             <div class="m-portlet__head-tools">
                 <a href="javascript:void(0);" class="m-nav__link btnArchive" onclick="archiveRecordShow();">
-                    <span class="m-nav__link-text">
+                    <span class="m-nav__link-text" id="archiveLabel">
                         Archive
                     </span>
                 </a>
